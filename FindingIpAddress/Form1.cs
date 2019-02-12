74d188c7048e7f5e122f9e4ce1085650f8fccd1d
golumall <sushilmall96@gmail.com>
2019-02-12 18:14:32 +0530

    first commit

---
+++ FindingIpAddress/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace FindingIpAddress
+{
+    public partial class Form1 : Form
+    {
+             public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress[] address = Dns.GetHostAddresses(txtsite.Text);
+                string str = string.Empty;
+                foreach (IPAddress ip in address)
+                {
+                    str += ip;
+                }
+                Form2 ob = new Form2(str);
+                ob.Show();
+            }
+            catch(Exception ee)
+            {
+                MessageBox.Show("Site does not Exist..");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 ob = new Form3();
+            ob.Show();
+             
+
+
+        }
+    }
+}
---
+++ FindingIpAddress/Form2.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FindingIpAddress
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(107, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ip Address Of Your given site:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblip
+            // 
+            this.lblip.AutoSize = true;
+            this.lblip.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblip.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblip.Location = new System.Drawing.Point(155, 96);
+            this.lblip.Name = "lblip";
+            this.lblip.Size = new System.Drawing.Size(0, 22);
+            this.lblip.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(513, 281);
+            this.Controls.Add(this.lblip);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Ip Address";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblip;
+    }
+}
